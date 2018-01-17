8561d6e7a5113f1eca8eee864c7efc337ad2c8ad
opurple <35505021+opurple@users.noreply.github.com>
2018-01-17 09:37:24 -0500

    Update sw.js

--- comple/sw.js
+++ comple/sw.js
@@ -46,11 +46,11 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Omnilife Peru';
   const options = {
     body: `${event.data.text()}`,
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
+    icon: 'images/omni.png',
+    badge: 'images/logo.png'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
